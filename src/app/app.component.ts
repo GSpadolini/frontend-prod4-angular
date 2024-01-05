91574ab68fb15bdb07c193f2bd6713412e27581e
xavi-mat <1102677+xavi-mat@users.noreply.github.com>
2024-01-05 15:31:01 +0100

    tidying

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,11 +11,14 @@
 
 import { Component, OnInit } from '@angular/core';
 import { environment } from "../environments/environment";
-import { getMessaging, getToken, onMessage } from "firebase/messaging";@Component({
+import { getMessaging, getToken, onMessage } from "firebase/messaging";
+
+@Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent implements OnInit {title = 'frontend-prod1';
   message:any = null;
   constructor() {}
