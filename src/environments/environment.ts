a3ff96107ffd939b0d1b9aaa11d90e99c191d83c
xavi-mat <1102677+xavi-mat@users.noreply.github.com>
2024-01-05 10:05:01 +0100

    update environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,8 @@ export const environment = {
       projectId: "frontend-5088a",
       storageBucket: "frontend-5088a.appspot.com",
       messagingSenderId: "417204717894",
-      appId: "1:417204717894:web:2c5f8e35bdc24eb13f3d47"
+      appId: "1:417204717894:web:2c5f8e35bdc24eb13f3d47",
+      vapidKey: "BEJsAudqcR5WG9bofwMi17KdSLJjMn9XK-LKNqgPhP2bsJBKG2SLJdXphvyU9cQjktVWzX-KVsNDgCfxN4_7gps"
    },
    production: false
 };
